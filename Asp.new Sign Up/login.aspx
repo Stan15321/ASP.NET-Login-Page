2f242ea6a0c233c9708483ec824c63919a9a378b
Stanislav Nikolov <stanislavnikolov567@gmail.com>
2022-10-10 22:01:31 +0300

    working on CSS

--- Asp.new Sign Up/login.aspx
+++ Asp.new Sign Up/login.aspx
@@ -4,11 +4,28 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>login</title>
+    <link href="style.css" rel="stylesheet" />
+
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
+        <section>
+            <img src="Stock/burger_1.jpg" class="panel" />
+        </section>
+        <div class="sec2">
+            <div class="container">
+                <div class="social">
+                    <asp:Image ID="Image1" runat="server" ImageUrl="~/Stock/422-4222871_640-x-640-15-facebook-small-icon-png.png" />
+                    <asp:Image ID="Image2" runat="server" ImageUrl="~/Stock/googleplus-logos-02.png" />
+                </div>
+                <div class="content">
+                    <h2>Sign Up</h2>
+                    <asp:TextBox ID="TextBox1" placeholder="Username" runat="server"></asp:TextBox><br />
+                    <asp:TextBox ID="TextBox2" placeholder="Password" runat="server"></asp:TextBox><br />
+                    <asp:Button ID="Submit" runat="server" Text="Sumbit" />
+                </div>
+            </div>
         </div>
     </form>
 </body>
--- Asp.new Sign Up/login.aspx.designer.cs
+++ Asp.new Sign Up/login.aspx.designer.cs
@@ -40,5 +40,32 @@ namespace Asp.new_Sign_Up
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Image Image2;
+
+        /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+
+        /// <summary>
+        /// TextBox2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+
+        /// <summary>
+        /// Submit control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Submit;
     }
 }
